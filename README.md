2782e960006cb02494263101a5e887f9ecb0bae5
AzalamIlham <ilhamazalam@gmail.com>
2024-09-30 08:55:42 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+"# InternShip-El-Abdia-Backend" 
--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import cookieParser from "cookie-parser";
 const app = express() 
 
 app.use(cors({
-    origin: ["https://stock-management-el-abdia.onrender.com"],
+    origin: ["http://localhost:5173"],
     methods: ['GET', 'POST', 'PUT', "DELETE"],
     credentials: true
 }))
